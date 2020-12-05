8ad08a307b9bed24162e7eb3f46b740b8bdcd4cd
VitaliiRadaiev <hf.chelenger@gmail.com>
2020-12-05 10:49:44 +0200

    fri

--- dist/js/main.js
+++ dist/js/main.js
@@ -1358,5 +1358,44 @@ let btn = document.querySelectorAll('.our-loans__tiggers').forEach((item) => {
 // ====  and header menu line correct =======================================================
 
 
+// === resource block ==================================================================
+if($('.resource__list').length>0) {
+	$('.resource__list').slick({
+		slidesToScroll: 1,
+		slidesToShow: 3,
+		prevArrow: '<div class="slick-arrow slick-prev"><span class=""><svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 443.52 443.52" style="enable-background:new 0 0 443.52 443.52;" xml:space="preserve"><g><path d="M143.492,221.863L336.226,29.129c6.663-6.664,6.663-17.468,0-24.132c-6.665-6.662-17.468-6.662-24.132,0l-204.8,204.8c-6.662,6.664-6.662,17.468,0,24.132l204.8,204.8c6.78,6.548,17.584,6.36,24.132-0.42c6.387-6.614,6.387-17.099,0-23.712L143.492,221.863z"/></g></svg></span></div>',
+		nextArrow: '<div class="slick-arrow slick-next"><span class=""><svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 443.52 443.52" style="enable-background:new 0 0 443.52 443.52;" xml:space="preserve"><g><path d="M336.226,209.591l-204.8-204.8c-6.78-6.548-17.584-6.36-24.132,0.42c-6.388,6.614-6.388,17.099,0,23.712l192.734,192.734L107.294,414.391c-6.663,6.664-6.663,17.468,0,24.132c6.665,6.663,17.468,6.663,24.132,0l204.8-204.8C342.889,227.058,342.889,216.255,336.226,209.591z"/></g></svg></span></div>',
+		
+		touchThreshold: 10,
+		//variableWidth: true,
+		//draggable: false,
+		// centerMode: true,
+		dots: true,
+		responsive: [
+		  {
+		    breakpoint: 992,
+		    settings: {
+		      slidesToShow: 2,
+		    }
+		  },
+		  {
+		    breakpoint: 768,
+		    settings: {
+		      slidesToShow: 1,
+		    }
+		  },
+		  {
+		    breakpoint: 576,
+		    settings: {
+		      slidesToShow: 1,
+		      arrows: false,
+		    }
+		  },
+		]
+	})
+}
+// === // resource block ==================================================================
+
+
 });
 
